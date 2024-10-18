13eda301c0c157f20c9c5871b2706fa94f718ef6
Clyde Watts <Clyde.Watts267@gmail.com>
2024-10-18 14:03:28 +0100

    Changed to use the new pack_example01.sql file

--- dbt_clearscape/models/pack/pack_example01.sql
+++ dbt_clearscape/models/pack/pack_example01.sql
@@ -2,16 +2,5 @@
 WITH data AS (
   SELECT * FROM {{source('clearscape_tests','ville_temperature')}}
 )
-SELECT * FROM {{Pack('data',delimiter=',',output_column='packed_date',include_column_name='true',target_columns='[1,4]',accumulate='sn')}} AS dt ;
-
-/*
-SELECT * FROM Pack (
-  ON data
-  USING
-  Delimiter (',')
-  OutputColumn ('packed_data')
-  IncludeColumnName ('true')
-  TargetColumns ('[1:4]')
-  Accumulate ('sn')
-) AS dt;
-*/
+SELECT * FROM {{Pack('data')}} AS dt 
+;
